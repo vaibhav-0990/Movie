bc0874095290b0d4db612869ebee4e0a080f34a7
Govind Kumar <govind85050@gmail.com>
2021-08-29 21:02:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+// import logo from './logo.svg';
+import './App.css';
+import { Switch, Route } from "react-router-dom";
+import { Home } from "./components/home/Home";
+import { MovieDetail } from "./components/moviedetail/MovieDetail";
+
+export function App() {
+  return (
+    <main>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/movie/:id" component={MovieDetail} />
+      </Switch>
+    </main>
+  );
+}
+
+export default App;
